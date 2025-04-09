2b6673a26acf9d3d12b9741feeb7e4aea8085a96
BrunoPaoloFigueroa <bruno.figueroa@usil.pe>
2025-04-09 11:59:52 -0500

    V2

--- src/main.cpp
+++ src/main.cpp
@@ -8,14 +8,19 @@ main (void){
   DDRD|=0xFF;
   DDRB&=~(0X1F);
   char a=0x00;
-  char o1=0x00;
-  char o2=0x00;
-  char rf=0x00;
+  char memoria1=0x00;
+  char memoria2=0x00;
+  char resultado=0x00;
+  char ram=0x00;
 
   
   while(1){
 
     char p=PIND&0X1F;
+    char u=ram%0x0A;
+    char d=ram/0x0A;
+    PORTB=(d<<4)|u;
+
 
     if(p==0x01){
 
