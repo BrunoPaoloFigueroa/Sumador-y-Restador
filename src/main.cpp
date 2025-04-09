55647b39d9ae803df9ad735a7b07f2441d737bff
BrunoPaoloFigueroa <bruno.figueroa@usil.pe>
2025-04-09 11:41:40 -0500

    V1

--- src/main.cpp
+++ src/main.cpp
@@ -5,10 +5,37 @@
 main (void){
 
 
-
+  DDRD|=0xFF;
+  DDRB&=~(0X1F);
+  char a=0x00;
+  char o1=0x00;
+  char o2=0x00;
+  char rf=0x00;
 
   
   while(1){
+
+    char p=PIND&0X1F;
+
+    if(p==0x01){
+
+      if(){
+
+
+
+
+      }
+
+
+
+
+
+
+    }
+
+
+
+
 
 
 
